f712c61f8a1e96ca915a538a4ab76b863fb92ef7
Martin Lear <m.lear09@gmail.com>
2014-06-01 21:00:39 -0500

    Complete

--- week_8_and_9/3_Ruby/rectangle_class/my_solution.rb
+++ week_8_and_9/3_Ruby/rectangle_class/my_solution.rb
@@ -1,9 +1,14 @@
 # U3.W8-9: Implement a Rectangle Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
+
+# create a instance method called area that will return the area of the Rectangle
+# create a instance method called perimeter that will return the perimeter of the Rectangle
+# create a instance method called diagonal that will return the diagnoal of the Rectangle
+# create a instance method called square? that will return a boolean
 
 
 
@@ -14,6 +19,22 @@ class Rectangle
   def initialize(width, height)
     @width  = width
     @height = height
+  end
+
+  def area
+  	return @width * @height
+  end
+
+  def perimeter
+  	return (@width + @height)*2
+  end
+
+  def diagonal
+  	return Math.sqrt( (@width**2) + (@height**2) )
+  end
+
+  def square?
+  	return (@width == @height)
   end
 
   def ==(other)
@@ -27,16 +48,20 @@ end
 
 # 4. Refactored Solution
 
-
+# nothing to refactor here
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
+#didn't have the change to do the tests as I was doing this as review last minute on sunday night at 9pm
 
 
 
 
 # 5. Reflection 
+
+# this one was pretty straight forward and didn't really give me a challenge beyond dusting the cobwebs off of basic math. 
+# not really sure what the def==(other) is doing there, but everything works that is being asked of me, so I am moving on for
+# now.
